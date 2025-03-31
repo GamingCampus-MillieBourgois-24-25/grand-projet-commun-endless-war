53e776a84979b1a1d510c547c2cbd13c1c0da3f7
JulenMYT <147490703+JulenMYT@users.noreply.github.com>
2025-03-31 14:00:05 +0200

    Organization dossier

---
+++ LOTR Survivor/Assets/Scripts/Player/Attacks/AttackBehaviour.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBehaviour : MonoBehaviour
+{
+    [Header("Paramètres")]
+    [SerializeField] protected float attackRange = 10f;
+    [SerializeField] protected int damage = 10;
+    [SerializeField] protected float attackCooldown = 1f;
+    [SerializeField] protected LayerMask enemyLayer;
+
+    protected float attackTimer;
+
+    protected virtual void Update()
+    {
+        attackTimer += Time.deltaTime;
+    }
+
+    protected bool CanAttack()
+    {
+        return attackTimer >= attackCooldown;
+    }
+
+    protected GameObject FindNearestEnemy()
+    {
+        Collider[] enemiesInRange = Physics.OverlapSphere(transform.position, attackRange, enemyLayer);
+
+        if (enemiesInRange.Length == 0) return null;
+
+        Collider nearestEnemy = enemiesInRange[0];
+        float shortestDistance = Vector3.Distance(transform.position, nearestEnemy.transform.position);
+
+        foreach (Collider enemy in enemiesInRange)
+        {
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distance < shortestDistance)
+            {
+                nearestEnemy = enemy;
+                shortestDistance = distance;
+            }
+        }
+
+        return nearestEnemy.gameObject;
+    }
+}
