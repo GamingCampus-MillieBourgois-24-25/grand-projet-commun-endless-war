7f87d494b130c741ce8328d240032bd9dda757d8
Portos2004 <fcardoso@gaming.tech>
2025-04-09 13:12:45 +0200

    HealAnims

--- LOTR Survivor/Assets/Scripts/Player/Heal/HealPickup.cs
+++ LOTR Survivor/Assets/Scripts/Player/Heal/HealPickup.cs
@@ -2,18 +2,43 @@ using UnityEngine;
 
 public class HealPickup : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject prefab;
     public int healAmount = 20;
+    private bool picked = false;
+
+    private void OnEnable()
+    {
+        picked = false;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !picked)
         {
             PlayerHealthBehaviour playerHealth = other.GetComponent<PlayerHealthBehaviour>();
             if (playerHealth != null)
             {
+                picked = true;
                 playerHealth.Heal(healAmount);
-                Destroy(gameObject);
+
+                if (animator != null)
+                {
+                    animator.SetTrigger("Picked");
+                }
+                else
+                {
+                    Delete();
+                }
             }
+        }
+    }
+
+    private void Delete()
+    {
+        if (ObjectPool.Instance != null)
+        {
+            ObjectPool.Instance.Despawn(gameObject, prefab);
         }
     }
 }
