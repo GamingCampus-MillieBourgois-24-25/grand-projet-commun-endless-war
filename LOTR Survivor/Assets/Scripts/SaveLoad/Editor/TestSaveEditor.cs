58ceaac386ac54be07967457ec7b8c241d9d13cf
Nonorrs <147596657+Nonorrs@users.noreply.github.com>
2025-04-18 17:07:32 +0200

    SaveLoadManager d'enfer

---
+++ LOTR Survivor/Assets/Scripts/SaveLoad/Editor/TestSaveEditor.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TooltipTestUI))]
+public class TestSave : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        TooltipTestUI script = (TooltipTestUI)target;
+
+        GUILayout.Space(10);
+        EditorGUILayout.LabelField("Debug Controls", EditorStyles.boldLabel);
+
+        if(GUILayout.Button("Save and Restart"))
+        {
+            script.SaveAndRestart();
+        }
+
+        if (GUILayout.Button("Reset Tooltips"))
+        {
+            script.ResetTooltips();
+        }
+    }
+}
--- LOTR Survivor/Assets/Scripts/SaveLoad/TooltipTestUI.cs
+++ LOTR Survivor/Assets/Scripts/SaveLoad/TooltipTestUI.cs
@@ -6,21 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class TooltipTestUI : MonoBehaviour
 {
-    [Header("Buttons")]
-    public Button saveAndRestartButton;
-    public Button resetTooltipsButton;
-
-    private void Start()
-    {
-        SaveLoadManager.LoadTooltipState();
-
-        if (saveAndRestartButton == null)
-            saveAndRestartButton.onClick.AddListener(SaveAndRestart);
-
-        if (resetTooltipsButton == null)
-            resetTooltipsButton.onClick.AddListener(ResetTooltips);
-    }
-
     public void ResetTooltips()
     {
         TooltipState.Instance.ResetTooltips();
