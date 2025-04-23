47c7b51a6192ae794d0b66517904bf8d3a075671
Nonorrs <147596657+Nonorrs@users.noreply.github.com>
2025-04-23 09:56:34 +0200

    SaveLoad sur main

--- LOTR Survivor/Assets/Scripts/SaveLoad/Editor/TestSaveEditor.cs
+++ LOTR Survivor/Assets/Scripts/SaveLoad/Editor/TestSaveEditor.cs
@@ -1,26 +1,26 @@
-//using UnityEditor;
-//using UnityEngine;
+using UnityEditor;
+using UnityEngine;
 
-//[CustomEditor(typeof(TooltipTestUI))]
-//public class TestSave : Editor
-//{
-//    public override void OnInspectorGUI()
-//    {
-//        base.OnInspectorGUI();
+[CustomEditor(typeof(TooltipTestUI))]
+public class TestSave : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
 
-//        TooltipTestUI script = (TooltipTestUI)target;
+        TooltipTestUI script = (TooltipTestUI)target;
 
-//        GUILayout.Space(10);
-//        EditorGUILayout.LabelField("Debug Controls", EditorStyles.boldLabel);
+        GUILayout.Space(10);
+        EditorGUILayout.LabelField("Debug Controls", EditorStyles.boldLabel);
 
-//        if(GUILayout.Button("Save and Restart"))
-//        {
-//            script.SaveAndRestart();
-//        }
+        if (GUILayout.Button("Save and Restart"))
+        {
+            script.SaveAndRestart();
+        }
 
-//        if (GUILayout.Button("Reset Tooltips"))
-//        {
-//            script.ResetTooltips();
-//        }
-//    }
-//}
+        if (GUILayout.Button("Reset Tooltips"))
+        {
+            script.ResetTooltips();
+        }
+    }
+}
