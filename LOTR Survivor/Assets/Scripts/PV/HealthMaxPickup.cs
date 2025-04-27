e9525be7f14cedcadf8fe4902019b51d0e812417
Nonorrs <147596657+Nonorrs@users.noreply.github.com>
2025-04-27 15:46:19 +0200

    Petit objet de merde fonctionnel

--- LOTR Survivor/Assets/Scripts/PV/HealthMaxPickup.cs
+++ LOTR Survivor/Assets/Scripts/PV/HealthMaxPickup.cs
@@ -2,18 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//public class HealthMaxPickup : MonoBehaviour
+//{
+//    [SerializeField] private float healthIncreasePercentage = 20f;
+
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        PlayerHealthBehaviour playerHealth = other.GetComponent<PlayerHealthBehaviour>();
+//        if (playerHealth != null )
+//        {
+//            playerHealth.IncreaseMaxHealthByPercentage( healthIncreasePercentage );
+//            PVMaxEvents.PickHPObject(healthIncreasePercentage);
+//            Destroy(gameObject);
+//        }
+//    }
+//}
+
 public class HealthMaxPickup : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private SphereCollider capsuleCollider;
+    [SerializeField] private Transform cube;
     [SerializeField] private float healthIncreasePercentage = 20f;
+
+    private bool picked = false;
+
+    private void Awake()
+    {
+        capsuleCollider = GetComponent<SphereCollider>();
+    }
+
+    private void OnEnable()
+    {
+        picked = false;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        PlayerHealthBehaviour playerHealth = other.GetComponent<PlayerHealthBehaviour>();
-        if (playerHealth != null )
+        if (other.CompareTag("Player") && !picked)
         {
-            playerHealth.IncreaseMaxHealthByPercentage( healthIncreasePercentage );
-            PVMaxEvents.PickHPObject(healthIncreasePercentage);
-            Destroy(gameObject);
+            picked = true;
+
+            PlayerHealthBehaviour playerHealth = other.GetComponent<PlayerHealthBehaviour>();
+            if (playerHealth != null)
+            {
+                playerHealth.IncreaseMaxHealthByPercentage(healthIncreasePercentage);
+                PVMaxEvents.PickHPObject(healthIncreasePercentage);
+                if (animator != null)
+                {
+                    animator.SetTrigger("Picked");
+                    Destroy(gameObject, 0.2f);
+                }
+            }
+        }
+    }
+
+
+    public void SetValue(float value)
+    {
+        healthIncreasePercentage = value;
+        if (cube != null)
+        {
+            cube.transform.localScale *= Mathf.Sqrt(value / 20f);
         }
     }
 }
+
